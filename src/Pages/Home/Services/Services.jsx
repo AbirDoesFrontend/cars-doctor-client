6319d5e093ffdc1155a5bae5ce37bf7982cb265f
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-05-25 12:30:39 +0600

    Functional Asceding and Desceding Button

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,35 +1,42 @@
 import { useEffect, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
-
 const Services = () => {
+  const [services, setServices] = useState([]);
+  const [asceding, setAsceding] = useState(true);
 
-    const [services, setServices] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:5000/services?sort=${asceding ? 'asc' : 'desc' }`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, [asceding]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    }, [])
-    
-
-    return (
-        <div className="bg-base-200 pt-5">
-            <div className="text-center space-y-5">
-                <h3 className="text-2xl font-bold text-orange-600">Service</h3>
-                <h2 className="text-5xl">Our Service Area</h2>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br />words which don&apos;t look even slightly believable. </p>
-            </div>
-            <div className="grid grid-cols-3 sm:grid-col-2 lg:grid-col-3 gap-6 justify-center p-12">
-                {
-                    services.map(service => <ServiceCard 
-                        key={service._id}
-                        service={service}
-                    />)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-base-200 pt-5">
+      <div className="text-center space-y-5">
+        <h3 className="text-2xl font-bold text-orange-600">Service</h3>
+        <h2 className="text-5xl">Our Service Area</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised <br />
+          words which don&apos;t look even slightly believable.{" "}
+        </p>
+      </div>
+      <div className="text-center mt-5 mb-5">
+        <button
+          className="btn btn-primary"
+          onClick={() => setAsceding(!asceding)}
+        >
+          {asceding ? "Price : High to Low" : "Price : Low to High"}
+        </button>
+      </div>
+      <div className="grid grid-cols-3 sm:grid-col-2 lg:grid-col-3 gap-6 justify-center p-12">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
